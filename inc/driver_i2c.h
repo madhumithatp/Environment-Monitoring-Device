52ca723b349aa20c7af33f17f1b7ed5c07440188
deepesh <deso6761@colorado.edu>
2019-03-08 14:43:00 -0700

    Adding i2c driver files

---
+++ inc/driver_i2c.h
@@ -0,0 +1,8 @@
+/*
+@File Name	: i2c_driver.h
+@Description: I2C file Wrapper function  
+@Author		: Deepesh Sonigra 
+@Author		: Madhumitha Tolakanahalli
+@Date		: 03/8/2019
+*/
+
---
+++ src/driver_i2c.c
@@ -0,0 +1,10 @@
+
+
+/*
+@File Name	: myI2C.h
+@Description: I2C file Wrapper function  
+@Author		: Deepesh Sonigra 
+@Author		: Madhumitha Tolakanahalli
+@Date		: 03/8/2019
+
+*/
